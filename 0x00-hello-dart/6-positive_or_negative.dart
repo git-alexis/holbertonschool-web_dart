a40a91ea52c854c0adb671102fedd5eaaf43e4b7
git-alexis <alex59.billemont@gmail.com>
2026-02-20 15:06:46 +0100

    Ajout d'un main affichant un message en fonction du signe d'un nombre

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  int number = int.parse(args[0]);
+  if (number > 0) {
+    print('$number is positive');
+  }
+  else if (number == 0) {
+    print('$number is zero');
+  }
+  else if (number < 0) {
+    print('$number is negative');
+  }
+}
