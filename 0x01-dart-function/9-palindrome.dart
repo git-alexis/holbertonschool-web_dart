b20556685783e59a07423f3fe56f1855cfc8acec
git-alexis <alex59.billemont@gmail.com>
2026-03-01 19:33:53 +0100

    Correction du cas : string avec un caractère spécial ('\n')

--- 0x01-dart-function/9-palindrome.dart
+++ 0x01-dart-function/9-palindrome.dart
@@ -1,18 +1,12 @@
 bool isPalindrome(String s) {
-  String substringDebut = "";
-  String substringFin = "";
-  int length = 0;
-
   if (s.length < 3) {
     return false;
   }
 
-  length = (s.length / 2).round();
-  substringDebut = s.substring(0, length);
-  substringFin = s.substring(length - 1, s.length);
+  int length = s.length ~/ 2;
 
   for (int i = 0; i < length; i++) {
-    if (substringDebut[i] != substringFin[length - 1 - i]) {
+    if (s[i] != s[s.length - 1 - i]) {
       return false;
     }
   }
