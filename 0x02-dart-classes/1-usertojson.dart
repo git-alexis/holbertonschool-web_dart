8b346aa414daad15c7f800005fa3b79eb4a4f4a2
git-alexis <alex59.billemont@gmail.com>
2026-03-13 12:32:40 +0100

    Ajout de la classe User avec les propriétés 'name / age / height' et la méthode 'toJson' renvoyant la représentation json de User

---
+++ 0x02-dart-classes/1-usertojson.dart
@@ -0,0 +1,15 @@
+class User {
+  String? name;
+  int? age;
+  double? height;
+
+  User({this.name, this.age, this.height});
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "age": age,
+      "height": height
+    };
+  }
+}
