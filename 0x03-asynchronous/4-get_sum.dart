ff257aa54bd5dacd3c5bffccf0be9d4de243e03c
git-alexis <alex59.billemont@gmail.com>
2026-03-23 20:05:40 +0100

    Ajout de la fonction 'calculateTotal()' qui renvoie la somme du prix des produits d'un user en utilisant les fonctions 'fetchUserData(), fetchUserOrders() et fetchProductPrice()' ou -1 en cas d'erreur

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,3 +1,25 @@
+import 'dart:convert';
 import '4-util.dart';
 
+Future<double> calculateTotal() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> user = jsonDecode(userData);
+    String userId = user['id'];
 
+    String ordersData = await fetchUserOrders(userId);
+    List<dynamic> orders = jsonDecode(ordersData);
+
+    double total = 0;
+
+    for (var product in orders) {
+      String priceData = await fetchProductPrice(product);
+      double price = (jsonDecode(priceData)).toDouble();
+      total += price;
+    }
+
+    return total;
+  } catch (error) {
+    return -1;
+  }
+}
